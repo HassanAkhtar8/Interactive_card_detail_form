aa500b8099f5508921fa0bd214a0e6d0654d4615
Hassan Akhtar <hassanakhtar@Hassans-MacBook-Air.local>
2024-12-21 16:01:24 +0500

    changed class names

--- src/App.jsx
+++ src/App.jsx
@@ -10,40 +10,29 @@ const onSubmit = (e)=>{
     <>
       <div className='hero'>
       <img className='backImg' src={back} alt="card back" />
-      <div className='psuedo'>
+      <div className='frontImgContainer'>
            <img className='frontImg' src={front} alt="card front" />
-           <span className='cnum'>0000 0000 0000 0000</span>
-           <span className='cvv'>Jake Paul</span>
-           <img className='cardlogo' src={sv} alt="card logo" />
-           <span className='cexpire'>12 / 25</span>
+           <span className='cardNumber'>0000 0000 0000 0000</span>
+           <span className='cardName'>Jake Paul</span>
+           <img className='cardLogo' src={sv} alt="card logo" />
+           <span className='cardExpire'>12 / 25</span>
       </div>
     
-   
-
       </div>
-
-
-
-
-
-
-
-
-
 
       <div className='form'>
 
       <form onSubmit={onSubmit}> 
 
-      <p>CARDHOLDERS NAME</p>
+      <p>CARDHOLDER NAME</p>
       <input type="text" placeholder='e.g. Ali'/>
       <p>CARD NUMBER</p>
       <input placeholder='e.g. 1234 5678 9123 0000' />
-      <p>EXP. Date(MM/YY) <span>CVC</span> </p>
+      <p>EXP. Date (MM/YY) <span>CVC</span> </p>
       <span>
-        <input className='arzi' type="number" placeholder='MM' />
-        <input className='arzi' type="number" placeholder='YY' />
-        <input className='arzi' type="number" placeholder='e.g. 123' />
+        <input className='input' type="number" placeholder='MM' />
+        <input className='input' type="number" placeholder='YY' />
+        <input className='input' type="number" placeholder='e.g. 123' />
       </span>
       <button>Confirm</button>
 
